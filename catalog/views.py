cbcf930a2d8e5318560682bad3475441436b0d90
Sergey <sergey.tikin94@gmail.com>
2024-08-29 18:07:56 +0300

    create function home, contacts

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(requests):
+    return render(requests, "home.html")
+
+
+def contacts(requests):
+    return render(requests, "contacts.html")
