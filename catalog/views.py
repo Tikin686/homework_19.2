ea3b38b96efb2f63dbcd225f7a662db64ef6146a
Sergey <sergey.tikin94@gmail.com>
2024-08-29 18:22:30 +0300

    create post

--- catalog/views.py
+++ catalog/views.py
@@ -7,4 +7,9 @@ def home(requests):
 
 
 def contacts(requests):
+    if requests.method == "POST":
+        name = requests.POST.get("name")
+        phone = requests.POST.get("phone")
+        message = requests.POST.get("message")
+        print(f"{name} {phone} {message}")
     return render(requests, "contacts.html")
