ba1264959f4e5c6cfebbee3807242332d7d6ef05
Sergey <sergey.tikin94@gmail.com>
2024-09-22 19:44:02 +0300

    fix

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import product_list, contacts, product_detail
+from catalog.views import ContactsTemplateView, ProductDetailView, ProductListView
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", product_list, name="product_list"),
-    path("contacts/", contacts, name="contacts"),
-    path("catalog/<int:pk>/", product_detail, name="product_detail")
+    path("", ProductListView.as_view(), name="product_list"),
+    path("contacts/", ContactsTemplateView.as_view(), name="contacts"),
+    path("catalog/<int:pk>/", ProductDetailView.as_view(), name="product_detail")
 ]
